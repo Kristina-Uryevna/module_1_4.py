97c707e8d405174b5970ed98df871918f6f3704b
Kristina-Uryevna <mironovakristina.ur@gmail.com>
2024-08-13 22:46:59 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_sring = input('Введите имя Вашей собаки: ')
+print(len(my_sring))
+print(my_sring.upper())
+print(my_sring.lower())
+print(my_sring.replace(' ', ''))
+print(my_sring[0])
+print(my_sring[-1])
